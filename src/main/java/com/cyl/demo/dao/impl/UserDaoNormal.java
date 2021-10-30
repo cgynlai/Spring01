8c076bd3304b558fcaea554c351a986ea4e32cf0
cgynlai <cgynlai@gmail.com>
2021-10-30 13:18:16 +0800

    created UserDao

---
+++ src/main/java/com/cyl/demo/dao/impl/UserDaoNormal.java
@@ -0,0 +1,13 @@
+package com.cyl.demo.dao.impl;
+
+import com.cyl.demo.dao.UserDao;
+
+public class UserDaoNormal implements UserDao {
+
+	public void add() {
+		System.out.println("add user to db ...");
+		
+	}
+  
+
+}
